504bd3126a93aa43b55b27ab7f656cd8f93e550b
atelierbram <bramdeh@gmail.com>
2019-11-16 21:26:35 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# atelier-schemes-alacritty
-colorschemes for Alacritty terminal
+# Atelier-Schemes Alacritty
+Colorschemes for Alacritty terminal
+
+### Installation
+Copy one of the colorscheme/config files in the `config/` folder to your `$HOME/.config/alacritty/` folder and rename it to `alacritty.yml`
+One might have to edit some preferences in the config, like the value for font-family, which is a consequence of having the colors not separate from, but in the same file, as the rest of the config.
+
+### Resources
+- Preview of how Atelier-Schemes themes can look like at [http://atelierbram.github.io/syntax-highlighting/atelier-schemes/](http://atelierbram.github.io/syntax-highlighting/atelier-schemes/) or [http://atelierbram.github.io/syntax-highlighting/prism/demo/](http://atelierbram.github.io/syntax-highlighting/prism/demo/)
+
+### More colorschemes for Alacritty
+- [Base2Tone-alacritty](https://github.com/atelierbram/Base2Tone-alacritty)
+- [Base4Tone-alacritty](https://github.com/atelierbram/Base4Tone-alacritty)
---
+++ io.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+
+# write to file
+overwrite_to_file()
+{
+  base16-builder --scheme "db/schemes/atelier-cave.yml" --template "db/templates/alacritty/dark.ejs" > "config/atelier-cave-dark.yml"
+  base16-builder --scheme "db/schemes/atelier-cave.yml" --template "db/templates/alacritty/light.ejs" > "config/atelier-cave-light.yml"
+  base16-builder --scheme "db/schemes/atelier-dune.yml" --template "db/templates/alacritty/dark.ejs" > "config/atelier-dune-dark.yml"
+  base16-builder --scheme "db/schemes/atelier-dune.yml" --template "db/templates/alacritty/light.ejs" > "config/atelier-dune-light.yml"
+  base16-builder --scheme "db/schemes/atelier-estuary.yml" --template "db/templates/alacritty/dark.ejs" > "config/atelier-estuary-dark.yml"
+  base16-builder --scheme "db/schemes/atelier-estuary.yml" --template "db/templates/alacritty/light.ejs" > "config/atelier-estuary-light.yml"
+  base16-builder --scheme "db/schemes/atelier-forest.yml" --template "db/templates/alacritty/dark.ejs" > "config/atelier-forest-dark.yml"
+  base16-builder --scheme "db/schemes/atelier-forest.yml" --template "db/templates/alacritty/light.ejs" > "config/atelier-forest-light.yml"
+  base16-builder --scheme "db/schemes/atelier-heath.yml" --template "db/templates/alacritty/dark.ejs" > "config/atelier-heath-dark.yml"
+  base16-builder --scheme "db/schemes/atelier-heath.yml" --template "db/templates/alacritty/light.ejs" > "config/atelier-heath-light.yml"
+  base16-builder --scheme "db/schemes/atelier-lakeside.yml" --template "db/templates/alacritty/dark.ejs" > "config/atelier-lakeside-dark.yml"
+  base16-builder --scheme "db/schemes/atelier-lakeside.yml" --template "db/templates/alacritty/light.ejs" > "config/atelier-lakeside-light.yml"
+  base16-builder --scheme "db/schemes/atelier-plateau.yml" --template "db/templates/alacritty/dark.ejs" > "config/atelier-plateau-dark.yml"
+  base16-builder --scheme "db/schemes/atelier-plateau.yml" --template "db/templates/alacritty/light.ejs" > "config/atelier-plateau-light.yml"
+  base16-builder --scheme "db/schemes/atelier-savanna.yml" --template "db/templates/alacritty/dark.ejs" > "config/atelier-savanna-dark.yml"
+  base16-builder --scheme "db/schemes/atelier-savanna.yml" --template "db/templates/alacritty/light.ejs" > "config/atelier-savanna-light.yml"
+  base16-builder --scheme "db/schemes/atelier-seaside.yml" --template "db/templates/alacritty/dark.ejs" > "config/atelier-seaside-dark.yml"
+  base16-builder --scheme "db/schemes/atelier-seaside.yml" --template "db/templates/alacritty/light.ejs" > "config/atelier-seaside-light.yml"
+  base16-builder --scheme "db/schemes/atelier-sulphurpool.yml" --template "db/templates/alacritty/dark.ejs" > "config/atelier-sulphurpool-dark.yml"
+  base16-builder --scheme "db/schemes/atelier-sulphurpool.yml" --template "db/templates/alacritty/light.ejs" > "config/atelier-sulphurpool-light.yml"
+
+#  cp "config/atelier-sulphurpool-dark.yml" "/Users/bram/.config/alacritty/alacritty.yml"
+#  cp "config/atelier-sulphurpool-light.yml" "/Users/bram/.config/alacritty/alacritty.yml"
+}
+
+# execute it
+overwrite_to_file
